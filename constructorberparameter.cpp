4201fdb3b6f58f9b94fd3820323861a68fd93fd1
Raraandinii <raraandinib@gmail.com>
2025-05-13 19:16:17 +0700

    constructor dengan parameter

---
+++ constructorberparameter.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class mahasiswa{
+public:
+// Constructror dengan parameter
+mahasiswa(int nim, string nama);
+};
+
+//Definisi constructor dengan parameter
+mahasiswa::mahasiswa(int nim, string nama) {
+    cout << "Constructor Dengan Parameter Terpanggil" << endl;
+    cout << "NIM : " << nim << endl;
+    cout << "Nama : " << nama << endl;
+}
+
+int main() {
+    // memanggil constructor dengan parameter
+    mahasiswa mhs(12345, "Pascal");
+    return 0;
+}
